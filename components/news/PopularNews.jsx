690a0677843f433a8eed4a603edfa0dc438edade
Anton Almishev <antonalmishev@abv.bg>
2025-07-03 12:30:41 +0300

    test1

--- components/news/PopularNews.jsx
+++ components/news/PopularNews.jsx
@@ -15,7 +15,7 @@ const PopularNews = async ({ type, news }) => {
     return (
         <div className='w-full pb-8 mt-5'>
             <div className='flex flex-col w-full gap-y-[14px]'>
-                <Title title="Popular news" />
+                <Title title="Популярни новини" />
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-3 sm:gap-3 lg:gap-x-3'>
                     {
                         popularNews.length > 0 && popularNews.map((item, i) => {
